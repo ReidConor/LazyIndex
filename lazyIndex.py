fbb5fb84278dec42f5e5fcf46e928677ed8fde59
Conor Reid <conor.reid@ucdconnect.ie>
2018-05-28 21:09:52 +0100

    call conversion function

--- lazyIndex.py
+++ lazyIndex.py
@@ -1,5 +1,15 @@
 #this is the main module that will be run each time, taking the pdf as an argument
 import os
+import sys
+from modules import formatConversion
 
 if __name__ == "__main__":
     os.system('clear')
+
+    if (len(sys.argv) < 2):
+        print ("No pdf supplied. Exiting")
+        exit(1)
+
+    pathToFile = sys.argv[1]
+    outputFile, numberOfPages = formatConversion.convertPdfToText(pathToFile)
+    
