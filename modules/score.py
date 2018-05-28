46f2df79613766de92275651825de7f52c802b77
Conor Reid <conor.reid@ucdconnect.ie>
2018-05-28 22:21:24 +0100

    add stubs for some functions to implement

--- modules/score.py
+++ modules/score.py
@@ -1,1 +1,18 @@
-#this module will hold the required functions to score a document in text format 
+#this module will hold the required functions to score a document in text format
+#could be handy
+
+def getWordCount():
+    '''
+    Total words
+    '''
+    wordCount = 0
+    return (wordCount)
+
+
+def getWordLengths():
+    '''
+    A dict of word lengths of the form
+    {length:countOfWordsOfThatLenght}
+    '''
+    wordLenghts={}
+    return (wordLenghts)
